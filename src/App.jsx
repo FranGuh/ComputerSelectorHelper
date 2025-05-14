7a1e78795ec36cb058a846f3dc3c67c7921f7bee
Gustavo Francisco <franguho@gmail.com>
2025-05-14 17:34:46 -0600

    Version 1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <BrowserRouter>
       <h1>Computer Selector <span className='title__alter'>Helper</span></h1>
       <AppRoutes />
+          <Analytics/>
       <h5>Coded by @Fran</h5>
     </BrowserRouter>
+
   )
 }
 
